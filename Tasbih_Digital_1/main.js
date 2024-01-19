9b39f4d57e27cc91a3157973500328add91545cc
Bayueka018 <96391215+Bayueka018@users.noreply.github.com>
2024-01-19 16:24:07 +0700

    Add files via upload

---
+++ Tasbih_Digital_1/main.js
@@ -0,0 +1,12 @@
+
+var count = 0;
+
+function incre() {
+  count ++;
+  document.getElementById('tnum').innerHTML = count;
+}
+
+function reset() {
+  count = 0;
+  document.getElementById('tnum').innerHTML = count;
+}
